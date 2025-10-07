69393e150f16d39f2a903a8951e12111e5f8de11
NikolayMihaylov-BG <68066865+NikolayMihaylov-BG@users.noreply.github.com>
2025-10-07 22:00:04 +0300

    ddasdad

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/', function () {
+    \Illuminate\Support\Facades\Log::debug("niko test");
     return view('welcome');
 });
 
-Auth::routes();
+//Auth::routes();
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
